bbeb710173f331ce472a793434e68e92ef00e5ae
At Lay <atlaydoposangma@gmail.com>
2025-07-28 13:35:32 +0000

    i have added icon.png and changed my logo.png with same name

--- src/components/JsonLdSchema.tsx
+++ src/components/JsonLdSchema.tsx
@@ -28,7 +28,7 @@ export function JsonLdSchema() {
       '@type': 'SearchAction',
       target: {
         '@type': 'EntryPoint',
-        urlTemplate': `${baseUrl}/search?q={search_term_string}`,
+        urlTemplate: `${baseUrl}/search?q={search_term_string}`,
       },
       'query-input': 'required name=search_term_string',
     },
