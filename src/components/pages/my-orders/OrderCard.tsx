08bf0c683575c077134142601f69335058ef151f
At Lay <atlaydoposangma@gmail.com>
2025-08-04 18:55:19 +0000

    ReferenceError: useMemo is not defined
    Source
    src/components/pages/my-o

--- src/components/pages/my-orders/OrderCard.tsx
+++ src/components/pages/my-orders/OrderCard.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { format } from 'date-fns';
